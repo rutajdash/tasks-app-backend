c016f6a452fab83d2efcf5c0c8f2bb15bb8f57ef
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2023-02-13 23:15:54 +0530

    feat: error ahndle task controller

--- task_controller.py
+++ task_controller.py
@@ -5,122 +5,164 @@ task_controller = Blueprint('task_controller', __name__, url_prefix='/task')
 
 @task_controller.route('/<int:task_id>', methods=['GET'])
 def get(task_id):
-    if (task_id >= len(df) or task_id < 0 or df.empty == True or df.iloc[task_id].isnull().values.any() == True):
+    try:
+        if (task_id >= len(df) or task_id < 0 or df.empty == True or df.iloc[task_id].isnull().values.any() == True):
+            return {
+                'code': 404,
+                'status': 'error',
+                'message': 'Task not found.'
+            }
+        
         return {
-            'code': 404,
-            'status': 'error',
-            'message': 'Task not found.'
+            'code': 200,
+            'status': 'success',
+            'data': df.loc[task_id].to_dict()
         }
-    
-    return {
-        'code': 200,
-        'status': 'success',
-        'data': df.loc[task_id].to_dict()
-    }
+    except:
+        return {
+            'code': 500,
+            'status': 'error',
+            'message': 'Error: Unable to get task.'
+        }
 
 @task_controller.route('/all', methods=['GET'])
 def list():
-    return {
-        'code': 200,
-        'status': 'success',
-        'data': df.to_dict('index')
-    }
+    try:
+        return {
+            'code': 200,
+            'status': 'success',
+            'data': df.to_dict('index')
+        }
+    except:
+        return {
+            'code': 500,
+            'status': 'error',
+            'message': 'Error: Unable to list tasks.'
+        }
 
 @task_controller.route('', methods=['POST'])
 def add():
-    data = request.get_json()
+    try:
+        data = request.get_json()
 
-    if (data['task'] == ''):
+        if (data['task'] == ''):
+            return {
+                'code': 400,
+                'status': 'error',
+                'message': 'Task cannot be empty.'
+            }
+        
+        if (data['status'] != 'incomplete' and data['status'] != 'complete'):
+            return {
+                'code': 400,
+                'status': 'error',
+                'message': 'Status is not valid.'
+            }
+
+        df.loc[len(df.index)] = [data['task'], data['status']]
+        df.to_csv('data.csv', index=True)
+
         return {
-            'code': 400,
-            'status': 'error',
-            'message': 'Task cannot be empty.'
+            'code': 200,
+            'status': 'success',
+            'message': 'Task added successfully.'
         }
-    
-    if (data['status'] != 'incomplete' and data['status'] != 'complete'):
+    except:
         return {
-            'code': 400,
+            'code': 500,
             'status': 'error',
-            'message': 'Status is not valid.'
+            'message': 'Error: Unable to add task.'
         }
-
-    df.loc[len(df.index)] = [data['task'], data['status']]
-    df.to_csv('data.csv', index=True)
-
-    return {
-        'code': 200,
-        'status': 'success',
-        'message': 'Task added successfully.'
-    }
 
 @task_controller.route('/<int:task_id>/description', methods=['PUT'])
 def update_status(task_id):
-    data = request.get_json()
+    try:
+        data = request.get_json()
 
-    if (task_id >= len(df) or task_id < 0 or df.empty == True or df.iloc[task_id].isnull().values.any() == True):
+        if (task_id >= len(df) or task_id < 0 or df.empty == True or df.iloc[task_id].isnull().values.any() == True):
+            return {
+                'code': 404,
+                'status': 'error',
+                'message': 'Task not found.'
+            }
+
+        if (data['task'] == ''):
+            return {
+                'code': 400,
+                'status': 'error',
+                'message': 'Task cannot be empty.'
+            }
+
+        df.loc[df.index == task_id, 'task'] = data['task']
+        df.to_csv('data.csv', index=True)
+
         return {
-            'code': 404,
-            'status': 'error',
-            'message': 'Task not found.'
+            'code': 200,
+            'status': 'success',
+            'message': 'Task updated successfully.'
         }
-
-    if (data['task'] == ''):
+    except:
         return {
-            'code': 400,
+            'code': 500,
             'status': 'error',
-            'message': 'Task cannot be empty.'
+            'message': 'Error: Unable to update task.'
         }
-
-    df.loc[df.index == task_id, 'task'] = data['task']
-    df.to_csv('data.csv', index=True)
-
-    return {
-        'code': 200,
-        'status': 'success',
-        'message': 'Task updated successfully.'
-    }
 
 @task_controller.route('/<int:task_id>/status', methods=['PUT'])
 def update_task(task_id):
-    data = request.get_json()
+    try:
+        data = request.get_json()
 
-    if (task_id >= len(df) or task_id < 0 or df.empty == True or df.iloc[task_id].isnull().values.any() == True):
+        if (task_id >= len(df) or task_id < 0 or df.empty == True or df.iloc[task_id].isnull().values.any() == True):
+            return {
+                'code': 404,
+                'status': 'error',
+                'message': 'Task not found.'
+            }
+
+        if (data['status'] != 'incomplete' and data['status'] != 'complete'):
+            return {
+                'code': 404,
+                'status': 'error',
+                'message': 'Status is not valid.'
+            }
+
+        df.loc[df.index == task_id, 'status'] = data['status']
+        df.to_csv('data.csv', index=True)
+
         return {
-            'code': 404,
-            'status': 'error',
-            'message': 'Task not found.'
+            'code': 200,
+            'status': 'success',
+            'message': 'Task updated successfully.'
         }
-
-    if (data['status'] != 'incomplete' and data['status'] != 'complete'):
+    except:
         return {
-            'code': 404,
+            'code': 500,
             'status': 'error',
-            'message': 'Status is not valid.'
+            'message': 'Error: Unable to update task.'
         }
-
-    df.loc[df.index == task_id, 'status'] = data['status']
-    df.to_csv('data.csv', index=True)
-
-    return {
-        'code': 200,
-        'status': 'success',
-        'message': 'Task updated successfully.'
-    }
 
 @task_controller.route('/<int:task_id>', methods=['DELETE'])
 def delete(task_id):
-    if (task_id >= len(df) or task_id < 0 or df.empty == True or df.iloc[task_id].isnull().values.any() == True):
+    try:
+        if (task_id >= len(df) or task_id < 0 or df.empty == True or df.iloc[task_id].isnull().values.any() == True):
+            return {
+                'code': 404,
+                'status': 'error',
+                'message': 'Task not found.'
+            }
+
+        df.drop(task_id, inplace=True)
+        df.to_csv('data.csv', index=True)
+
         return {
-            'code': 404,
-            'status': 'error',
-            'message': 'Task not found.'
+            'code': 200,
+            'status': 'success',
+            'message': 'Task deleted successfully.'
         }
-
-    df.drop(task_id, inplace=True)
-    df.to_csv('data.csv', index=True)
-
-    return {
-        'code': 200,
-        'status': 'success',
-        'message': 'Task deleted successfully.'
-    }
+    except:
+        return {
+            'code': 500,
+            'status': 'error',
+            'message': 'Error: Unable to delete task.'
+        }
